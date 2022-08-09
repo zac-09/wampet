49fe06a0b5d24b017abd5e8d0b51b55e8b6f2574
zac <isaacmubiru99@gmail.com>
2022-08-09 10:34:40 +0300

    feat: CI/CD

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f8d9d365992660e0098d";
+/******/ 	var hotCurrentHash = "82e47c217bb7ba5b1f20";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
