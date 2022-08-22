749e107b4d84eeca0a26e865c5d9427197669b75
zac <isaacmubiru99@gmail.com>
2022-08-22 15:28:58 +0300

    fix: auth controller

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a039ac532b04c7856e1f";
+/******/ 	var hotCurrentHash = "7dfcbacb65efe66820d1";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
