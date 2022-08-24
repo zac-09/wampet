37bc52ab146c6b23b50890405a80ca94e9535978
zac <isaacmubiru99@gmail.com>
2022-08-24 19:42:11 +0300

    fix:contract issue

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 //Set up mongoose connection
 const mongoose = require('mongoose');
-const mongoDB = 'mongodb+srv://wampetic:NhXoZufqxn3VdSt8@cluster0.8ki4koc.mongodb.net/?retryWrites=true&w=majority';
+const mongoDB = 'mongodb+srv://zac:test1234@cluster0.fcv5s9w.mongodb.net/?retryWrites=true&w=majority';
 mongoose.connect(mongoDB,{ useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 module.exports = mongoose;
