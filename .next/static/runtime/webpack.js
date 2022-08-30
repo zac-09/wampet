6e176ac16a43362e0cb566fa388e4db248e8f6a4
zac <isaacmubiru99@gmail.com>
2022-08-30 15:23:42 +0300

    fix: authenticate middleware

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "8f170d736f6d07f9dad4";
+/******/ 	var hotCurrentHash = "9d57df83d2c2b29d33f1";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
