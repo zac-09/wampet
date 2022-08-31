caf2e26f76491f3466addeef1bd97cfe1c7bc785
wampet <wampet232@gmail.com>
2022-08-31 13:35:51 +0300

    grammatical error correction

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -112,9 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-
-/******/ 	var hotCurrentHash = "c8e16d1f5b11f0cce240";
-
+/******/ 	var hotCurrentHash = "dba8e65b3b878b06f24f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- controllers/candidate.js
+++ controllers/candidate.js
@@ -14,11 +14,11 @@ module.exports = {
 			from: process.env.EMAIL,
 			to: req.body.email,
 			subject: req.body.election_name + 'Registration',
-			html: 'Congragulations you have been registered for  ' + req.body.election_name + ' election.',
+			html: 'Congratulations you have been registered for  ' + req.body.election_name + ' election.',
 		};
 		transporter.sendMail(mailOptions, function (err, info) {
 			if (err) {
-				res.json({ status: 'error', message: 'mail error', data: null });
+				res.json({ status: 'Error', message: 'Mail error', data: null });
 				console.log(err);
 			} else console.log(info);
 			res.json({ status: 'success', message: 'Email sent successfully!!!', data: null });
